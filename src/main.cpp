3d4d06b6a24b60e5f299aa4575e4aa96ed52f63e
Kelenna Udo <udo.kelenna.uma.15@gmail.com>
2025-06-12 09:38:01 -0300

    added ultrasonic distance sensing functionality to the led contol - (bar graph style)

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,33 @@
-//www.elegoo.com
-//2016.12.9 
+// Arduino board: This project is designed for the Arduino Uno board.
+// Libraries: This code uses the Arduino core library for basic functionality.
 #include <Arduino.h>
-
+#include <HCSR04.h>
 int tDelay = 100;
 int latchPin = 11;      // (11) ST_CP [RCK] on 74HC595
 int clockPin = 9;      // (9) SH_CP [SCK] on 74HC595
 int dataPin = 12;     // (12) DS [S1] on 74HC595
+int echoPin = 7;      // (7) Echo pin for ultrasonic sensor
+int trigPin = 8;      // (8) Trigger pin for ultrasonic sensor
+long distance{}; // Variable to store the distance measured by the ultrasonic sensor
+int mappedValue = 0; // Variable to store the mapped value of the distance
+byte leds = 0; // Variable to store the state of the LEDs
 
-byte leds = 0;
-
+UltraSonicDistanceSensor ultrasonicSensor(trigPin, echoPin); 
 // Updates the 74HC595 shift register with the current LED state.
+
+//function prototypes
 void updateShiftRegister();
 void all_left_to_right();
 void one_back_and_forth();
 void two_inward_outward();
 void pulse_wave();
+void activate_map_leds(int &ledIndex, byte &leds);
+int map(int ditance);
 
 void setup() 
 {
+  Serial.begin(9600); // Initialize serial communication for debugging
+  Serial.println("74HC595 LED Control with Ultrasonic Sensor");
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);  
   pinMode(clockPin, OUTPUT);
@@ -25,22 +35,30 @@ void setup()
 
 void loop() 
 {
-  all_left_to_right(); // Turn on all LEDs one by one from left to right
-  one_back_and_forth(); // Turn on LEDs one by one from left to right, then right to left (ironman style)
-  two_inward_outward(); // Two LEDs Moving Inward and Outward 
-  pulse_wave(); // Pulse Wave / Growing Bar
 
-  //#########################################################
-  
-  // Clear for next cycle
   leds = 0;
-  updateShiftRegister();
-  delay(tDelay);
+
+  // Measure distance using the ultrasonic sensor
+  distance = ultrasonicSensor.measureDistanceCm(); // Measure distance in cm
+  mappedValue = map(distance); // Map the distance to a value between 0 and 7
+  Serial.print("Distance: "); 
+  Serial.print(distance); // Print the distance
+  Serial.print(" cm, Mapped Value: ");  
+  Serial.println(mappedValue); // Print the mapped value
+  // Clear for next cycle
+  if (mappedValue >= 0)
+    activate_map_leds(mappedValue, leds); // Activate the LED corresponding to the mapped value
+  else {
+    Serial.println("Invalid distance, no LEDs activated.");
+  }
+  updateShiftRegister(); // Update the shift register with the current LED state
+  delay(500);
 }
 
 // ######### End of the main loop
 // #####################################################
 // FUNCTIONS
+
 
 // Updates the 74HC595 shift register with the current LED state.
 void updateShiftRegister(){
@@ -58,8 +76,27 @@ void updateShiftRegister(){
        digitalWrite(latchPin, HIGH);
        latchState = HIGH;
    }
-   digitalWrite(latchPin, HIGH);
+
 }
+
+// Maps the distance measured by the ultrasonic sensor to a value between 0 and 7
+int map(int distance) {
+  // Map the distance to a value between 0 and 7
+  // Assuming the distance is in cm and we want to map it to 8 LEDs
+  if (distance < 0 || distance > 400) {
+    return -1; // Invalid distance
+  }
+  return map(distance, 0, 75, 0, 7);
+}
+
+// Activate the LED corresponding to the mapped value
+void activate_map_leds(int &mappedValue, byte &leds) {
+  for(int i =0; i <= mappedValue; i++) {
+    leds |= (1 << i); // Set the bit corresponding to the mapped value
+  }
+}
+
+
 
 
 //Turn on all LEDs one by one from left to right
@@ -78,13 +115,13 @@ void one_back_and_forth(){
   for (int i = 0; i < 8; i++) {
   leds = 1 << i;
   updateShiftRegister();
-  delay(100);
+  delay(tDelay);
   }
   
   for (int i = 6; i >= 1; i--) {
     leds = 1 << i;
     updateShiftRegister();
-    delay(100);
+    delay(tDelay);
   }
 }
 
@@ -95,13 +132,13 @@ void two_inward_outward(){
   for (int i = 0; i < 4; i++) {
     leds = (1 << i) | (1 << (7 - i));
     updateShiftRegister();
-    delay(100);
+    delay(tDelay);
   }
   // Moving outward (center to edges)
   for (int i = 2; i >= 0; i--) {
     leds = (1 << i) | (1 << (7 - i));
     updateShiftRegister();
-    delay(100);
+    delay(tDelay);
   }
 }
 
@@ -112,6 +149,6 @@ void pulse_wave(){
     leds |= (1 << (3 - i)); // Center-left
     leds |= (1 << (4 + i)); // Center-right
     updateShiftRegister();
-    delay(100);
+    delay(tDelay);
   }
 }
